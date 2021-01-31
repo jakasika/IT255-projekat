9fed573329bc717db254260ce13dfa7ce02c1c1a
jaksa <jaksa98@hotmail.com>
2021-01-31 15:32:04 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDm4AfpI73cLnTns1rvSXFnNCdJvx4WC_M",
+    authDomain: "it255-webshop.firebaseapp.com",
+    projectId: "it255-webshop",
+    storageBucket: "it255-webshop.appspot.com",
+    messagingSenderId: "1028909953032",
+    appId: "1:1028909953032:web:a419f907aea0706b86e1bd",
+    measurementId: "G-GH1KJEFEB2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDm4AfpI73cLnTns1rvSXFnNCdJvx4WC_M",
+    authDomain: "it255-webshop.firebaseapp.com",
+    projectId: "it255-webshop",
+    storageBucket: "it255-webshop.appspot.com",
+    messagingSenderId: "1028909953032",
+    appId: "1:1028909953032:web:a419f907aea0706b86e1bd",
+    measurementId: "G-GH1KJEFEB2"
+  }
 };
 
 /*
